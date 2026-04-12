6b7644086ddeafd67a22af30fbccd00df3833ff6
Mauro Brandão <mrbrjr@gmail.com>
2026-04-12 18:03:51 -0300

    add vim as default editor in setup.sh
    I don't know if exposing my .gitconfig file is the safest way to go,
    so I'm adding the command in setup.sh for now.

--- setup.sh
+++ setup.sh
@@ -12,3 +12,6 @@ ln -sf "$DOTFILES/.vimrc" ~/.vimrc
 ln -sf "$DOTFILES/kitty/kitty.conf" ~/.config/kitty/kitty.conf
 
 echo "Symbolic links created successfully."
+
+# Git related config
+git config --global core.editor "vim"
